9af8ea387c00af9e17dba34963aad3f2b1317fd3
ugurbizkit <uguryoruk26@gmail.com>
2022-03-24 22:28:03 +0300

    cleanıup

---
+++ Assets/Game/Core - Usecases/Waves/EnemyManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public SpawnPoints points;
+
+    public int Count { get; set; }
+
+    public void Execute(Spawnables spawnables)
+    {
+        var spawnCount = points.points.Count;
+
+        for (int i = 0; i < spawnCount; i++)
+        {
+            var obj = Instantiate(spawnables.items[0].obj, points.spawnPoint);
+            obj.transform.position = points.points[i].position;
+        }
+    }
+}
