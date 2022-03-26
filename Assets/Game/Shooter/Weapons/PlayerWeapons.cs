f673e54aedbb1bded9d4c5aae6f05d62656503b8
ugurbizkit <uguryoruk26@gmail.com>
2022-03-26 21:41:15 +0300

    resources

---
+++ Assets/Game/Shooter/Weapons/PlayerWeapons.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeapons : MonoBehaviour
+{
+    public List<PlayerWeapon> weapons;
+}
+
+[System.Serializable]
+public class PlayerWeapon
+{
+    public bool unlocked = false;
+    public WeaponAsset asset;
+}
