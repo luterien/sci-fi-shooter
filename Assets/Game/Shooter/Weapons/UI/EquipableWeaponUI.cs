db5721fcab1b29329d6bfa1d6b70fd145dab01ad
ugurbizkit <uguryoruk26@gmail.com>
2022-03-27 13:38:33 +0300

    fx

--- Assets/Game/Shooter/Weapons/UI/EquipableWeaponUI.cs
+++ Assets/Game/Shooter/Weapons/UI/EquipableWeaponUI.cs
@@ -25,7 +25,6 @@ public class EquipableWeaponUI : MonoBehaviour
 
     public void SetSelected(bool selected)
     {
-        print(selected);
         border.gameObject.SetActive(selected);
     }
 }
